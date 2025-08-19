8e2253df21234ba2d5c9ecb172a88b581bf2a56b
laotzee <laotzeabreu@proton.me>
2025-08-19 12:53:24 -0400

    Create models for Prompt and Tag

--- simply_write/base/models.py
+++ simply_write/base/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Tag(models.Model):
+    """
+    Models different categories for content
+    """
+    name = models.CharField(max_length=15)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Prompt(models.Model):
+    """
+    Models writing prompts properties
+    """
+    body = models.CharField(max_length=50)
+    tag = models.ManyToManyField(Tag, related_name='tags')
+    likes = models.ManyToManyField(User, related_name='liked_prompts')
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.body
