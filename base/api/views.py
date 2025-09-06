b5a98ad06ec12b8931537194caf8c81365841ff2
laotzee <laotzeabreu@proton.me>
2025-09-06 14:30:24 -0400

    Standardize json responses in api views. Change on how the JS was tapping into v1/prompt/ api response

--- base/api/views.py
+++ base/api/views.py
@@ -1,24 +1,47 @@
-from rest_framework.generics import get_object_or_404
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .serializers import PromptSerializer, TagSerializer
+from .serializers import PromptSerializer
 from ..models import Prompt, Tag
+from django.http import HttpResponse, HttpRequest
 
-@api_view(['GET', 'POST'])
-def get_prompt(request):
+def standard_json(response_status: int, data: dict | None, message:str=None) -> dict:
     """
-    returns a single model prompt as JSON
+    Formats the json responses for the API
+    :param response_status: either 1 to indicate success or 0 for error
+    :param data: either a dictionary containing the response data or None
+    :param message: optional message for the response
+    :return: formatted dictionary
     """
-    prompt = Prompt.objects.order_by('?').first()
-    obj = PromptSerializer(prompt)
-    return Response(obj.data)
+    if response_status != 1 and response_status !=0:
+        raise ValueError(f'response_status must be either 1 for success or 0 for error')
 
-@api_view(['GET', 'POST'])
-def generic_func(request):
-    """
-    Generic func for base
-    """
-    obj = {
-        'template_obj': 'Currently using a templatte'
+    success = 'success'
+    error = 'error'
+    response = {
+        'status': success if response_status == 1 else error,
+        'data': data,
+        'message': message,
     }
-    return Response(obj.data)
+    return response
+
+@api_view(['GET'])
+def get_prompt(request: HttpRequest) -> Response:
+    """returns a single model prompt as JSON"""
+    prompt = Prompt.objects.order_by('?').first()
+    if prompt:
+        serialized_prompt = PromptSerializer(prompt)
+        response = standard_json(1, serialized_prompt.data)
+        return Response(response, status.HTTP_200_OK)
+    else:
+        response = standard_json(0,
+                                 None,
+                                 'Apologies, something went wrong',)
+        return Response(response, status.HTTP_404_NOT_FOUND)
+
+@api_view(['GET', 'POST'])
+def generic_func(request: HttpRequest) -> Response:
+    """Generic func for an API"""
+
+    response = standard_json(0, None)
+    return Response(response, status.HTTP_404_NOT_FOUND)
