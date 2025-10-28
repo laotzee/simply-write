abb460a2283502e02642c755b96cf6bba1cc97d8
laotzee <laotzeabreu@proton.me>
2025-10-28 19:15:19 -0400

    Change naming for API endpoint

--- base/api/urls.py
+++ base/api/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('v1/', views.get_prompt, name=''),
-    path('v1/prompt/', views.get_prompt, name='prompt'),
+    path('v1/prompts/', views.get_prompt, name='prompts'),
 ]
 
 
